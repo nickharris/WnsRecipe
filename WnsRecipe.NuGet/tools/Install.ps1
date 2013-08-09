8b427d8de90934f07bc4bc88dcd5f37786ba1daa
Nick harris <nickha@microsoft.com>
2013-08-08 17:47:35 -0700

    Revert "Preview from June 2012"
    This reverts commit 5afce5eba5a3092dd1f3a65f5eedec08be492d62.

--- WnsRecipe.NuGet/tools/Install.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$path = [System.IO.Path]
-$readmeFile = $path::Combine($path::GetDirectoryName($project.FileName), "App_Readme\WnsRecipe.Readme.htm")
-$DTE.ItemOperations.Navigate($readmeFile, [EnvDTE.vsNavigateOptions]::vsNavigateOptionsNewWindow)
