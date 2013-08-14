58882dffb951463d62e5f958d2b5ee29c932d20a
Nick harris <nickha@microsoft.com>
2013-08-14 16:29:15 -0700

    folder layout

--- source/WnsRecipe.NuGet/tools/Install.ps1
+++ source/WnsRecipe.NuGet/tools/Install.ps1
@@ -1,5 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
 
 $path = [System.IO.Path]
-$readmeFile = ""
+$readmeFile = "https://github.com/nickharris/WnsRecipe/blob/master/README.md"
 $DTE.ItemOperations.Navigate($readmeFile, [EnvDTE.vsNavigateOptions]::vsNavigateOptionsNewWindow)
